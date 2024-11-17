7ea87f80b986fee9af58e802ce3cda606623d4da
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-11-17 16:24:39 +0700

    add user seed

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User; // Perbaiki huruf "a" menjadi "A"
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin'), 
+        ]);
+        $admin->assignRole('admin');
+
+        $santri = User::create([
+            'name' => 'santri',
+            'email' => 'santri@example.com',
+            'password' => Hash::make('santri'), 
+        ]);
+        $santri->assignRole('santri'); 
+    }
+}
