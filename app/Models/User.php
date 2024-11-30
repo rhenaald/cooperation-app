ba7d2ad28ce441e5810d63e89fbbe17cd9dda4d4
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-11-30 13:05:10 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,13 @@ use App\Models\Transaction;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
 use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Auth;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
     use HasRoles;
@@ -52,15 +54,28 @@ class User extends Authenticatable
         return $this->hasMany(Transaction::class);
     }
 
-//     protected static function booted()
-// {
-//     static::created(function ($user) {
-//         // Jika user tidak memiliki role apapun atau tidak memiliki role 'admin'
-//         if ($user->roles->isEmpty() || !$user->hasRole('admin')) {
-//             // Tetapkan role 'santri' jika user belum memiliki role atau bukan admin
-//             $user->syncRoles(['santri']);
-//         }
-//     });
-// }
+    public function canAccessPanel(Panel $panel): bool
+    {
+        $user = Auth::user();
+        $roles = $this->roles->pluck('name');
 
+        if ($panel->getId() === 'Admin_dashboard' && $this->roles->contains('id', 1)){
+            return true;
+        }
+        else if ($panel->getId() === 'user_dashboard' && $this->roles->contains('id', 2)){
+            return true;
+        }else
+            return false;
+    }
+
+    protected static function booted()
+    {
+        static::created(function ($user) {
+            if (!$user->roles->where('id', 1)) {
+                // Jika user tidak memiliki role dengan ID 1, tetapkan role 'santri'
+                $user->assignRole('santri');
+            }
+        });
+    }
+    
 }
