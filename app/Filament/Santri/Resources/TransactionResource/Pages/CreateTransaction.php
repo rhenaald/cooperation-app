991ffb5c973ba3a269d08ceb2e37ea8020b9cbf5
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-11-30 20:00:20 +0700

    Create CreateTransaction.php

---
+++ app/Filament/Santri/Resources/TransactionResource/Pages/CreateTransaction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Santri\Resources\TransactionResource\Pages;
+
+use App\Filament\Santri\Resources\TransactionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTransaction extends CreateRecord
+{
+    protected static string $resource = TransactionResource::class;
+}
