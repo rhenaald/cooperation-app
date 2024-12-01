575cec681a9537582a545f5b6da0efd77130d345
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-12-01 14:56:03 +0700

    Update TransactionResource.php

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -98,7 +98,7 @@ class TransactionResource extends Resource
     {
         return [
             'index' => Pages\ListTransactions::route('/'),
-            'create' => Pages\CreateTransaction::route('/create'),
+            // 'create' => Pages\CreateTransaction::route('/create'),
             // 'edit' => Pages\EditTransaction::route('/{record}/edit'),
         ];
     }
