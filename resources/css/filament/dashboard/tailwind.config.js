36a1495aee09c16840d82954f40ecd738d5b055f
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-12-01 07:33:32 +0700

    Create tailwind.config.js

---
+++ resources/css/filament/dashboard/tailwind.config.js
@@ -0,0 +1,10 @@
+import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
+
+export default {
+    presets: [preset],
+    content: [
+        './app/Filament/Santri/**/*.php',
+        './resources/views/filament/santri/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+    ],
+}
